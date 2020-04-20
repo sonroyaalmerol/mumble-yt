7456c808840106fc6817b37f8e23fd3ec5f12cef
Son Roy Almerol <sonroya.almerol@gmail.com>
2020-04-20 22:37:46 +0800

    fix yargs

--- utils/mumble.js
+++ utils/mumble.js
@@ -1,11 +1,9 @@
 const noodleJS = require('noodle.js')
 const options = require('./yargs')
 
-module.exports = () => {
-  return new noodleJS({
-    url: options.url.split(":")[0] || '127.0.0.1',
-    port: options.url.split(":")[1] || '64738',
-    name: options.name || 'YoutubeBot',
-    password: options.password || ''
-  })
-} 
+module.exports = new noodleJS({
+  url: options.url.split(":")[0] || '127.0.0.1',
+  port: options.url.split(":")[1] || '64738',
+  name: options.name || 'YoutubeBot',
+  password: options.password || ''
+})
