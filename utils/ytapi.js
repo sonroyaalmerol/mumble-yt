3d2763553790a049479194cad761dee27a20aba9
Son Roy Almerol <sonroya.almerol@gmail.com>
2020-04-20 17:25:19 +0800

    add more api

--- utils/ytapi.js
+++ utils/ytapi.js
@@ -3,5 +3,6 @@ const { YouTube } = require('popyt')
 module.exports = [
   new YouTube('AIzaSyDR91-KkUJS0NymwsXMGgjbrrzIBW6Ry2Q'),
   new YouTube('AIzaSyArBFTlqt7nt91J70adVZuf4aGz3IrC6u8'),
-  new YouTube('AIzaSyC419OR1p_FoFTGr3PHhMNNcoKPOQPTYPQ')
+  new YouTube('AIzaSyC419OR1p_FoFTGr3PHhMNNcoKPOQPTYPQ'),
+  new YouTube('AIzaSyCkgq6-UHFk4Wg3xCHNBVuwmByOKS7ZKUQ')
 ]
