4c562db7d07e542d9278b219ae30b2edeb9796d6
Son Roy Almerol <sonroya.almerol@gmail.com>
2020-04-23 21:35:44 +0800

    change youtube link validator

--- utils/ytapi.js
+++ utils/ytapi.js
@@ -1,16 +1,15 @@
 const url = require('yt-scraper')
 const search = require('scrape-youtube')
-const { validateUrl } = require('youtube-validate')
+const youtubeUrl = require('youtube-url')
 
 module.exports = async (query) => {
   var details = {}
-  try {
-    await validateUrl(query)
+  if(youtubeUrl.valid(query)) {
     var tmp = await url.videoInfo(query)
     details.title = tmp.title
     details.url = tmp.url
     details.duration = tmp.length
-  } catch (err) {
+  } else {
     var tmp = await search.search(query, { limit: 1, type: 'video' })
     details.title = tmp[0].title
     details.url = tmp[0].link
